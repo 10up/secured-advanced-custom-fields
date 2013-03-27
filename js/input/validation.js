a2b67ba7055ea159029da75c0c58e44672431e23
Elliot Condon <e@elliotcondon.com>
2013-03-27 16:55:00 +1100

    Fixed JS validation bug
    http://support.advancedcustomfields.com/discussion/5352/flex-content-required-field-error
    Signed-off-by: Elliot Condon <e@elliotcondon.com>

--- js/input/validation.js
+++ js/input/validation.js
@@ -55,7 +55,7 @@
 				var id = div.find('.wp-editor-area').attr('id'),
 					editor = tinyMCE.get( id );
 
-				if( ! editor.getContent() )
+				if( editor && !editor.getContent() )
 				{
 					div.data('validation', false);
 				}
