bdab1f78bf9e4b73cd983ffaa9f7b6c4dae59dce
Elliot Condon <e@elliotcondon.com>
2013-05-05 14:31:27 +1000

    * User field: Add sanitisation into update_value function to allow for array / object
    Signed-off-by: Elliot Condon <e@elliotcondon.com>

--- core/fields/user.php
+++ core/fields/user.php
@@ -302,6 +302,46 @@ class acf_field_user extends acf_field
 		
 	}
 	
+	
+	/*
+	*  update_value()
+	*
+	*  This filter is appied to the $value before it is updated in the db
+	*
+	*  @type	filter
+	*  @since	3.6
+	*  @date	23/01/13
+	*
+	*  @param	$value - the value which will be saved in the database
+	*  @param	$post_id - the $post_id of which the value will be saved
+	*  @param	$field - the field array holding all the field options
+	*
+	*  @return	$value - the modified value
+	*/
+	
+	function update_value( $value, $post_id, $field )
+	{
+		// array?
+		if( is_array($value) )
+		{
+			if( isset($value['ID']) )
+			{
+				$value = $value['ID'];
+			}	
+		}
+		
+		// object?
+		if( is_object($value) )
+		{
+			if( isset($value->ID) )
+			{
+				$value = $value->ID;
+			}
+		}
+		
+		return $value;
+	}
+	
 		
 }
 
