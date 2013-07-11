af33afabfd80691114769edad3b366d0c92e8874
Elliot Condon <e@elliotcondon.com>
2013-07-11 16:35:20 +1000

    Select field: Fixing choices ':' explode function
    Signed-off-by: Elliot Condon <e@elliotcondon.com>

--- core/fields/select.php
+++ core/fields/select.php
@@ -330,9 +330,9 @@ class acf_field_select extends acf_field
 			// key => value
 			foreach($field['choices'] as $choice)
 			{
-				if(strpos($choice, ':') !== false)
+				if(strpos($choice, ' : ') !== false)
 				{
-					$choice = explode(':', $choice);
+					$choice = explode(' : ', $choice);
 					$new_choices[ trim($choice[0]) ] = trim($choice[1]);
 				}
 				else
