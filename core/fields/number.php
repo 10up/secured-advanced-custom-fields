dd6f7157995f52d2f9b8a385b0f41687d8809cec
Elliot Condon <e@elliotcondon.com>
2013-07-20 14:31:26 +1000

    Number field: Adding new options for placeholder, prepend and append
    Signed-off-by: Elliot Condon <e@elliotcondon.com>

--- core/fields/number.php
+++ core/fields/number.php
@@ -19,9 +19,12 @@ class acf_field_number extends acf_field
 		$this->label = __("Number",'acf');
 		$this->defaults = array(
 			'default_value'	=>	'',
-			'min' => '',
-			'max' => '',
-			'step' => ''
+			'min'			=>	'',
+			'max'			=>	'',
+			'step'			=>	'',
+			'placeholder'	=>	'',
+			'prepend'		=>	'',
+			'append'		=>	''
 		);
 		
 		
@@ -44,17 +47,35 @@ class acf_field_number extends acf_field
 	
 	function create_field( $field )
 	{
+		// vars
+		$o = array( 'id', 'class', 'min', 'max', 'step', 'name', 'value' );
+		$e = '';
+		
+		
 		// step
 		if( !$field['step'] )
 		{
 			$field['step'] = 'any';
 		}
 		
+		// prepend
+		if( $field['prepend'] !== "" )
+		{
+			$field['class'] .= ' acf-is-prepended';
+			$e .= '<div class="acf-input-prepend">' . $field['prepend'] . '</div>';
+		}
 		
-		// vars
-		$o = array( 'id', 'class', 'min', 'max', 'step', 'name', 'value' );
 		
-		$e = '<input type="number"';
+		// append
+		if( $field['append'] !== "" )
+		{
+			$field['class'] .= ' acf-is-appended';
+			$e .= '<div class="acf-input-append">' . $field['append'] . '</div>';
+		}
+		
+		
+		$e .= '<div class="acf-input-wrap">';
+		$e .= '<input type="number"';
 		
 		foreach( $o as $k )
 		{
@@ -62,6 +83,7 @@ class acf_field_number extends acf_field
 		}
 		
 		$e .= ' />';
+		$e .= '</div>';
 		
 		
 		// return
@@ -156,7 +178,48 @@ class acf_field_number extends acf_field
 		?>
 	</td>
 </tr>
-
+<tr class="field_option field_option_<?php echo $this->name; ?>">
+	<td class="label">
+		<label><?php _e("Placeholder Text",'acf'); ?></label>
+	</td>
+	<td>
+		<?php 
+		do_action('acf/create_field', array(
+			'type'	=>	'text',
+			'name'	=>	'fields[' .$key.'][placeholder]',
+			'value'	=>	$field['placeholder'],
+		));
+		?>
+	</td>
+</tr>
+<tr class="field_option field_option_<?php echo $this->name; ?>">
+	<td class="label">
+		<label><?php _e("Prepend",'acf'); ?></label>
+	</td>
+	<td>
+		<?php 
+		do_action('acf/create_field', array(
+			'type'	=>	'text',
+			'name'	=>	'fields[' .$key.'][prepend]',
+			'value'	=>	$field['prepend'],
+		));
+		?>
+	</td>
+</tr>
+<tr class="field_option field_option_<?php echo $this->name; ?>">
+	<td class="label">
+		<label><?php _e("Append",'acf'); ?></label>
+	</td>
+	<td>
+		<?php 
+		do_action('acf/create_field', array(
+			'type'	=>	'text',
+			'name'	=>	'fields[' .$key.'][append]',
+			'value'	=>	$field['append'],
+		));
+		?>
+	</td>
+</tr>
 		<?php
 	}
 	
