cea94104065ed7d858659710dac85950e21a4077
Elliot Condon <e@elliotcondon.com>
2013-09-25 17:17:40 +1000

    Location field: Removed empty functions
    Signed-off-by: Elliot Condon <e@elliotcondon.com>

--- core/fields/google-map.php
+++ core/fields/google-map.php
@@ -39,29 +39,6 @@ class acf_field_google_map extends acf_field
 	
 	
 	/*
-	*  load_field()
-	*  
-	*  This filter is appied to the $field after it is loaded from the database
-	*  
-	*  @type filter
-	*  @since 3.6
-	*  @date 23/01/13
-	*  
-	*  @param $field - the field array holding all the field options
-	*  
-	*  @return $field - the field array holding all the field options
-	*/
-	
-	function load_field( $field )
-	{
-				
-		
-		// return
-		return $field;
-	}
-	
-	
-	/*
 	*  create_field()
 	*
 	*  Create the HTML interface for your field
@@ -236,56 +213,6 @@ class acf_field_google_map extends acf_field
 		<?php
 		
 	}
-	
-	
-	/*
-	*  format_value()
-	*
-	*  This filter is appied to the $value after it is loaded from the db and before it is passed to the create_field action
-	*
-	*  @type	filter
-	*  @since	3.6
-	*  @date	23/01/13
-	*
-	*  @param	$value	- the value which was loaded from the database
-	*  @param	$post_id - the $post_id from which the value was loaded
-	*  @param	$field	- the field array holding all the field options
-	*
-	*  @return	$value	- the modified value
-	*/
-	
-	function format_value( $value, $post_id, $field )
-	{
-				
-		// return value
-		return $value;	
-	}
-	
-	
-	/*
-	*  format_value_for_api()
-	*
-	*  This filter is appied to the $value after it is loaded from the db and before it is passed back to the api functions such as the_field
-	*
-	*  @type	filter
-	*  @since	3.6
-	*  @date	23/01/13
-	*
-	*  @param	$value	- the value which was loaded from the database
-	*  @param	$post_id - the $post_id from which the value was loaded
-	*  @param	$field	- the field array holding all the field options
-	*
-	*  @return	$value	- the modified value
-	*/
-	
-	function format_value_for_api( $value, $post_id, $field )
-	{
-		
-		
-		// return
-		return $value;
-		
-	}	
 }
 
 new acf_field_google_map();
