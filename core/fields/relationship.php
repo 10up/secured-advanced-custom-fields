8dec8673574ab0b7789a92a847c54bd596770691
Elliot Condon <e@elliotcondon.com>
2014-02-07 13:15:28 +1100

    Added translation support to the post_type displayed in a relationship field result
    Signed-off-by: Elliot Condon <e@elliotcondon.com>

--- core/fields/relationship.php
+++ core/fields/relationship.php
@@ -291,7 +291,8 @@ class acf_field_relationship extends acf_field
 				
 				if( in_array('post_type', $field['result_elements']) )
 				{
-					$title .= get_post_type();
+					$post_type_object = get_post_type_object( get_post_type() );
+					$title .= $post_type_object->labels->singular_name;
 				}
 				
 				// WPML
