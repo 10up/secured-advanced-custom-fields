c9f332f3217853d9c522b9782cdeafff96a43f0e
Elliot Condon <e@elliotcondon.com>
2014-02-15 11:30:10 +1100

    Improved post_type code on the relationship items
    Signed-off-by: Elliot Condon <e@elliotcondon.com>

--- core/fields/relationship.php
+++ core/fields/relationship.php
@@ -492,8 +492,10 @@ class acf_field_relationship extends acf_field
 					
 					if( in_array('post_type', $field['result_elements']) )
 					{
-						$title .= $p->post_type;
+						$post_type_object = get_post_type_object( get_post_type() );
+						$title .= $post_type_object->labels->singular_name;
 					}
+					
 					
 					// WPML
 					if( defined('ICL_LANGUAGE_CODE') )
